5c89e55ff8b53b763455d7d149c473a7711fb451
Jano Kacer <jano.kacer@gmail.com>
2015-12-30 20:41:37 +0100

    added med_evtype.py

---
+++ helpers.py
@@ -0,0 +1,15 @@
+'''
+*********************************************************************
+HELPER METHODS
+*********************************************************************
+'''
+
+def complement(c, width=64):
+        return c ^ (2**width-1)
+
+def printHex(head, body):
+        print(head, end='')
+        for i in body:
+                print("{:02x}".format(i), end='')
+        print()
+
