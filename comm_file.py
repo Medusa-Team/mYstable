8535847e62861fde166b837f1cf03bc6e5509e6c
juraaj <juraj.kirka@gmail.com>
2017-01-23 12:22:17 +0100

    added check in comm_file.py method checkFile if actual path is path to file

--- comm_file.py
+++ comm_file.py
@@ -7,6 +7,11 @@ def getCommType():
 def checkFile(files):
 
     for file in files:
+
+        #check if path is path to file,
+        if os.path.isfile(file) is False:
+            raise AttributeError("Attribute " + file + " is not file")
+
         writeable = os.access(file, os.W_OK)  # check if it is possible to write into file
         if writeable is False:
             raise AttributeError("It is not possible to write into file '" + file + "'")
